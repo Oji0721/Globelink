7dafbd888e1ece7908760c26261b27c1ef9490ea
Cloudlink <90021248+Oji0721@users.noreply.github.com>
2024-10-15 09:38:12 +0900

    Create embedBuilder.js

---
+++ src/utils/utils/embedBuilder.js
@@ -0,0 +1,9 @@
+const { EmbedBuilder } = require('discord.js');
+
+function createEmbed(title, description, fields = [], color = 0x3498DB) {
+    return new EmbedBuilder()
+        .setTitle(title)
+        .setDescription(description)
+        .addFields(fields)
+        .setColor(color);
+}
