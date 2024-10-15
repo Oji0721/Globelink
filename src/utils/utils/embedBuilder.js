c12c3829988c8baa0eaf9b91c35ab4b9b1f5846d
Cloudlink <90021248+Oji0721@users.noreply.github.com>
2024-10-15 09:38:28 +0900

    Delete src/utils/utils directory

--- src/utils/utils/embedBuilder.js
+++
@@ -1,9 +0,0 @@
-const { EmbedBuilder } = require('discord.js');
-
-function createEmbed(title, description, fields = [], color = 0x3498DB) {
-    return new EmbedBuilder()
-        .setTitle(title)
-        .setDescription(description)
-        .addFields(fields)
-        .setColor(color);
-}
